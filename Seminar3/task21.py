1b91918477927dcf8b19b2e2f605fef5c0c81160
Sergey Antipov <ser_ok@mail.ru>
2023-05-27 15:17:34 +0300

    added Seminar 3 task 16

---
+++ Seminar3/task21.py
@@ -0,0 +1,25 @@
+import os
+os.system('clear') 
+
+'''
+Задача №21. Решение в группах
+Напишите программу для печати всех уникальных
+значений в словаре.
+Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+{"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
+":" S007 "}]
+Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+Примечание: Список словарей задан изначально.
+Пользователь его не вводит
+'''
+
+l = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
+     {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
+startValues = list()
+unicueValues = set()
+for i in l:
+    for (k, m) in i.items():
+        startValues.append(m)
+        unicueValues.add(m)
+print(f'все значения словаря:\n{startValues}\n')
+print(f'уникальные значения словаря:\n{unicueValues}\n')
